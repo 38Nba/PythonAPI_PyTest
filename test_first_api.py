be4f5968f2561a7af2c57e26be6ce6cf808ba59f
38Nba <99139703+38Nba@users.noreply.github.com>
2023-10-12 14:47:00 +0300

    Update test_first_api.py
    upload

--- test_first_api.py
+++ test_first_api.py
@@ -1,9 +1,15 @@
+import pytest
 import requests
 
+
 class TestFirstAPI:
-    def test_hello_world(self):
+    names = [("Nikita"), #проверка будет происходить три раза
+             ("Sasha"),
+             ("")]
+
+    @pytest.mark.parametrize('name', names) #указываем имя переменной в которой будем передавать данные
+    def test_hello_world(self, name): #добавили переменную имя в которой передается список с проверочными именами
         url = "https://playground.learnqa.ru/api/hello"
-        name = "Mark"
         data = {'name': name}
 
         response = requests.get(url, params=data)
@@ -13,6 +19,10 @@ class TestFirstAPI:
         response_dict = response.json()
         assert "answer" in response_dict, "There is no field 'answer' in the response" #проверяем имеется ли в словаре поле "answer", небходимо для того чтобы в дальнейшем к нему обпатиться
 
-        expected_response_text = f"Hello, {name}" #ожидаемый текст ответа
+        if len(name) == 0:
+            expected_response_text = "Hello, someone"
+        else:
+            expected_response_text = f"Hello, {name}" #ожидаемый текст ответа
+
         actual_response_text = response_dict["answer"] #реальный текст ответа
         assert expected_response_text == actual_response_text, "Actual text in the response is not correct" #сравниваем, если отличаются выводим ошибку
