f9f60d6b457166852015f737834f8897511f50fb
38Nba <99139703+38Nba@users.noreply.github.com>
2023-10-12 12:20:04 +0300

    Update get_test_two.py
    upload

--- get_test_two.py
+++ get_test_two.py
@@ -1,6 +1,5 @@
-from json.decoder import JSONDecodeError
+from json.decoder import JSONDecodeError #если выдает список ошибок можно импортировать декодер
 import requests
-
 
 response = requests.get("https://playground.learnqa.ru/api/get_text")
 try:
