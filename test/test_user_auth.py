ddb05bcf5839ef1ac3458fff06ccae4581cb203b
38Nba <99139703+38Nba@users.noreply.github.com>
2023-10-31 17:51:30 +0300

    Update test_user_auth.py
    update

--- test/test_user_auth.py
+++ test/test_user_auth.py
@@ -1,7 +1,8 @@
 import requests
 import pytest
+from lib.base_case import BaseCase
 
-class TestUserAuth:
+class TestUserAuth(BaseCase):
     exclude_params = [
         ("no_cookie"),
         ("no_token")
@@ -14,13 +15,9 @@ class TestUserAuth:
         }
         response1 = requests.post('https://playground.learnqa.ru/api/user/login', data=data)
 
-        # проверики на наличие определенных значений в куки, хэдерс и json
-        assert 'auth_sid' in response1.cookies, "There is no auth cookie in the response"
-        assert 'x-csrf-token' in response1.headers, "there is no CSRF token header in the response"
-        assert 'user_id' in response1.json(), 'There is no user id in the response'
-
-        self.auth_sid = response1.cookies.get("auth_sid")
-        self.token = response1.headers.get("x_csrf_token")
+        self.auth_sid = self.get_cookie(response1, "auth_sid")
+        self.token = self.get_header(response1, "x-csrf-token")
+        self.user_id_from_auth_method = self.get_json_value(response1, "user_id")
 
     def test_auth_user(self):
 
