1067f739201b24217039e1cbaba3719711393ccf
38Nba <99139703+38Nba@users.noreply.github.com>
2023-11-08 13:29:46 +0300

    Update base_case.py
    update

--- lib/base_case.py
+++ lib/base_case.py
@@ -1,16 +1,15 @@
 import json.decoder
-
 from requests import Response
 
-class BaseCase:
+class BaseCase: #
     def get_cookie(self, response: Response, cookie_name):
-        assert cookie_name in response.cookie, f"Cannot find cookie with name {cookie_name} in the last response"
-        return response.cookie[cookie_name]
-
+        assert cookie_name in response.cookies, f"Cannot find cookie with name {cookie_name} in the last response"
+        return response.cookies[cookie_name]
+#
     def get_header(self, response: Response, header_name):
-        assert header_name in response.cookie, f"Cannot find header with name {header_name} in the last response"
-        return response.header[header_name]
-
+        assert header_name in response.cookies, f"Cannot find header with name {header_name} in the last response"
+        return response.headers[header_name]
+#
     def get_json_value(self, response: Response, name):
         try:
             response_as_dict = response.json()
