f34cc8180def5ac701ac0920de3395b218557d53
38Nba <99139703+38Nba@users.noreply.github.com>
2023-11-09 15:34:09 +0300

    Update base_case.py
    update

--- lib/base_case.py
+++ lib/base_case.py
@@ -1,14 +1,15 @@
 import json.decoder
 from requests import Response
+from datetime import datetime
 
 class BaseCase: #
     def get_cookie(self, response: Response, cookie_name):
         assert cookie_name in response.cookies, f"Cannot find cookie with name {cookie_name} in the last response"
         return response.cookies[cookie_name]
 #
-    def get_header(self, response: Response, header_name):
-        assert header_name in response.cookies, f"Cannot find header with name {header_name} in the last response"
-        return response.headers[header_name]
+    def get_header(self, response: Response, headers_name):
+        assert headers_name in response.headers, f"Cannot find header with name {headers_name} in the last response"
+        return response.headers[headers_name]
 #
     def get_json_value(self, response: Response, name):
         try:
@@ -17,5 +18,18 @@ class BaseCase: #
             assert False, f"Response is not in JSON format. Response text is '{response.text}'"
 
         assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
-
         return response_as_dict[name]
+
+    def prepare_registration_date(self, email=None):
+        if email is None:
+            base_part = "learnqa"
+            domain = "example.com"
+            random_part = datetime.now().strftime("%m%d%Y%H%M%S")
+            email = f"{base_part}{random_part}@{domain}"
+        return {
+            'password': '123',
+            'username': 'learnqa',
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': email
+        }
