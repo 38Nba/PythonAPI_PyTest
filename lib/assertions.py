de13e922b14670e50d83ef53d1709ecd99aa26a2
38Nba <99139703+38Nba@users.noreply.github.com>
2023-11-09 15:34:02 +0300

    Update assertions.py
    update

--- lib/assertions.py
+++ lib/assertions.py
@@ -9,7 +9,7 @@ class Assertions:
         except json.JSONDecodeError:
             assert False, f"response is not in JSON format. Response text is '{response.text}'"
 
-        assert name in response_as_dict, f"Response JSON doesn't have key '{name}"
+        assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
         assert response_as_dict[name] == expected_value
 
     @staticmethod
@@ -19,16 +19,18 @@ class Assertions:
         except json.JSONDecodeError:
             assert False, f"response is not in JSON format. Response text is '{response.text}'"
 
-        assert name in response_as_dict, f"Response JSON doesn't have key '{name}"
+        assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
+
     @staticmethod
     def assert_json_has_keys(response: Response, names: list):
         try:
             response_as_dict = response.json()
         except json.JSONDecodeError:
-            assert False, f"response is not in JSON format. Response text is '{response.text}'"
+            assert False, f"Response is not in JSON format. Response text is '{response.text}'"
 
         for name in names:
-            assert name in response_as_dict, f"Response JSON doesn't have key '{name}"
+            assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
+
     @staticmethod
     def assert_json_has_not_key(response: Response, name):
         try:
